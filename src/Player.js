b66889802dca406d8291d25705fe89193e8fd993
Quentin-Allmn <92529322+Quentin-Allmn@users.noreply.github.com>
2022-04-04 13:54:44 +0200

    m

---
+++ src/Player.js
@@ -0,0 +1,75 @@
+class Player {
+
+    constructor(Tableau) {
+        this.scene = Tableau
+        this.pousse=false
+        this.player = this.scene.physics.add.sprite(100, 350, 'player');
+        this.player.key=0
+        this.player.setBounce(0.1).setVelocityX(0);
+        this.scene.physics.add.collider(this.player, this.scene.platforms);
+        this.scene.anims.create({
+            key: 'walk',
+            frames: this.scene.anims.generateFrameNames('player', {
+                prefix: 'robo_player_',
+                start: 2,
+                end: 3,
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.scene.anims.create({
+            key: 'idle',
+            frames: [{key: 'player', frame: 'robo_player_0'}],
+            frameRate: 10,
+
+        });
+        this.scene.anims.create({
+            key: 'jump',
+            frames: [{key: 'player', frame: 'robo_player_1'}],
+            frameRate: 10,
+            repeat:-1,
+
+        });
+        this.scene.physics.add.collider(this.player, this.scene.moves,this.force,null,this)
+        this.scene.physics.add.collider(this.player, this.scene.doors,this.isKey,null,this)
+        this.scene.physics.add.overlap(this.player, this.scene.key,this.getKey,null,this)
+
+    }
+    force(player,moves){
+        if(player.body.touching.left || player.body.touching.right) {
+            moves.setImmovable(false)
+            this.pousse = true
+        }
+        if(player.body.touching.down){
+            moves.setImmovable()
+            this.player.body.blocked.down=true //ici on fais croire a phaser que le sprite est static pour pouvoir sauter dessus (uniquement valable pour utiliser la fonction onFloor())
+        }
+    }
+
+    jump(){
+        this.player.setVelocityY(-350);
+        this.player.play('jump', true);
+        console.log(this.player.key)
+    }
+    moveRight(){
+        this.player.setVelocityX(200);
+        this.player.setFlipX(false);
+        if (this.player.body.onFloor()) {
+            this.player.play('walk', true)}
+    }
+    moveLeft(){
+        this.player.setVelocityX(-200);
+        if (this.player.body.onFloor()) {
+            this.player.play('walk', true)}
+        this.player.setFlipX(true);
+    }
+    stop(){
+        this.player.setVelocityX(0);
+        if (this.player.body.onFloor()) {
+            this.player.play('idle',true)
+        }
+    }
+
+
+
+}
--- src/Tableau.js
+++ src/Tableau.js
@@ -1,15 +1,39 @@
 class Tableau extends Phaser.Scene {
     preload() {
-        
+
+        this.load.image('background', 'assets/images/background.png');
+        this.load.atlas('player', 'assets/images/kenney_player.png', 'assets/images/kenney_player_atlas.json');
+        this.load.image('tiles', 'assets/tilesets/platformPack_tilesheet.png');
+        this.load.tilemapTiledJSON('map', 'assets/tilemaps/level1.json');
     }
 
     create() {
 
+        const backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0);
+        backgroundImage.setScale(2, 0.8);
+
+        const map = this.make.tilemap({key: 'map'});
+        const tileset = map.addTilesetImage('platformPack_tilesheet', 'tiles');
+        this.platforms = map.createStaticLayer('Platforms', tileset, 0, 200);
+        this.platforms.setCollisionByProperty({collides:true});
+        this.platforms.setCollisionByExclusion(-1, true);
     }
 
 
 
     update() {
+
+        if ((this.cursors.space.isDown || this.cursors.up.isDown) && this.player.player.body.onFloor()) {
+            this.player.jump()
+            console.log("oui")
+        }
+        if (this.cursors.left.isDown) {
+            this.player.moveLeft()
+        } else if (this.cursors.right.isDown) {
+            this.player.moveRight()
+        } else {
+            this.player.stop();
+        }
 
     }
 }
